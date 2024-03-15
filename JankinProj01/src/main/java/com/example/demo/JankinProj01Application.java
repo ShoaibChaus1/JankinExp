d54aac97ea167130325eccb0b2f05efff0266840
ShoaibChaus1 <102368361+ShoaibChaus1@users.noreply.github.com>
2024-03-15 15:04:29 +0530

    Adding

---
+++ JankinProj01/src/main/java/com/example/demo/JankinProj01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankinProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankinProj01Application.class, args);
+	}
+
+}
